e5709674b25bbef7fd055972ede525d8d81c908e
cswaney <colinswaney@gmail.com>
2021-04-11 23:11:58 -0400

    Setup Auth0

--- src/main.js
+++ src/main.js
@@ -2,6 +2,27 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// Import the Auth0 configuration
+import { domain, clientId, audience } from '../auth_config.json';
+
+// Import the Auth0 plugin
+import { Auth0Plugin } from './auth';
+
+// Install the Auth0 plugin
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  audience,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
+
+
 Vue.config.productionTip = false;
 
 new Vue({
