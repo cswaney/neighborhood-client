9ae51db9964fa31d7801c99a2f101053c9c9715d
cswaney <colinswaney@gmail.com>
2021-04-20 09:21:27 -0400

    Re-format auth files

--- src/auth/authGuard.js
+++ src/auth/authGuard.js
@@ -1,27 +1,27 @@
-import { getInstance } from "./authWrapper";
+import { getInstance } from './authWrapper';
 
 export const authGuard = (to, from, next) => {
-    const authService = getInstance();
+  const authService = getInstance();
 
-    const fn = () => {
-        // If the user is authenticated, continue to route
-        if (authService.isAuthenticated) {
-            return next();
-        }
-
-        // Else, login user
-        authService.loginWithRedirect({ appState: { targetUrl: to.fullPath } });
-    };
-
-    // If loading finished, check the auth state using `fn`
-    if (!authService.loading) {
-        return fn();
+  const fn = () => {
+    // If the user is authenticated, continue to route
+    if (authService.isAuthenticated) {
+      return next();
     }
 
-    // Check auth state if loading property changes
-    authService.$watch("loading", loading => {
-        if (loading === false) {
-            return fn();
-        }
-    });
+    // Else, login user
+    authService.loginWithRedirect({ appState: { targetUrl: to.fullPath } });
+  };
+
+  // If loading finished, check the auth state using `fn`
+  if (!authService.loading) {
+    return fn();
+  }
+
+  // Check auth state if loading property changes
+  authService.$watch('loading', (loading) => {
+    if (loading === false) {
+      return fn();
+    }
+  });
 };
