47f43ad95f8d7ab0549ad990285df77f7e758137
cswaney <colinswaney@gmail.com>
2021-04-20 09:23:39 -0400

    Create Event and Comment components
    Not currently using these...

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,8 +1,39 @@
 <template>
   <div>
-    <p>{{ text }}</p>
-    <p>{{ timestamp }}</p>
-    <p>{{ author }}</p>
+    <article v-if="!this.isReply" class="uk-comment uk-visible-toggle" tabindex="-1">
+        <header class="uk-comment-header uk-position-relative">
+            <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                <div class="uk-width-auto">
+                    <img class="uk-border-circle" src="../assets/images/avatar.jpg" width="80" height="80" alt="">
+                </div>
+                <div class="uk-width-expand">
+                    <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">{{ userName }}</a></h4>
+                    <p class="uk-comment-meta uk-margin-remove-top"><a class="uk-link-reset" href="#">12 days ago</a></p>
+                </div>
+            </div>
+            <div class="uk-position-top-right uk-position-small uk-hidden-hover"><a class="uk-link-muted" href="#">Reply</a></div>
+        </header>
+        <div class="uk-comment-body">
+            <p>{{ text }}</p>
+        </div>
+    </article>
+    <article v-if="this.isReply" class="uk-comment uk-comment-primary uk-visible-toggle" tabindex="-1">
+        <header class="uk-comment-header uk-position-relative">
+            <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                <div class="uk-width-auto">
+                    <img class="uk-border-circle" src="../assets/images/avatar.jpg" width="80" height="80" alt="">
+                </div>
+                <div class="uk-width-expand">
+                    <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">{{ userName }}</a></h4>
+                    <p class="uk-comment-meta uk-margin-remove-top"><a class="uk-link-reset" href="#">12 days ago</a></p>
+                </div>
+            </div>
+            <div class="uk-position-top-right uk-position-small uk-hidden-hover"><a class="uk-link-muted" href="#">Reply</a></div>
+        </header>
+        <div class="uk-comment-body">
+            <p>{{ text }}</p>
+        </div>
+    </article>
   </div>
 </template>
 
@@ -10,10 +41,22 @@
 export default {
   name: 'Comment',
   props: {
+    // comment: Object,
+    userId: String,
+    userName: String,
     text: String,
-    timestamp: String,
-    author: String
+    createdAt: String,
+    type: String,
   },
+  computed: {
+    daysAgo() {
+      // number of days between now and comment.createdAt
+      return 12
+    },
+    isReply() {
+      return this.type == 'reply'
+    },
+  }
 };
 </script>
 
--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
-    <h1>Event: {{ event.title }}</h1>
-    <p>Location: {{ event.location }}</p>
-    <p>Date: {{ event.date }}</p>
-    <p>{{ event.description }}</p>
+    <div class="uk-card uk-card-default uk-width-1-1@m uk-margin-medium">
+      <div class="uk-card-header uk-card-primary uk-padding-small">
+          <div class="uk-grid-small uk-flex-middle" uk-grid>
+              <!-- <div class="uk-width-auto">
+                  <img class="uk-border-circle" width="40" height="40" src="images/avatar.jpg">
+              </div> -->
+              <div class="uk-width-expand">
+                  <h3 class="uk-card-title uk-margin-remove-bottom">{{ event.headline }}</h3>
+                  <p class="uk-text-meta uk-margin-remove-top">
+                    {{ this.formattedDate }}
+                  </p>
+              </div>
+          </div>
+      </div>
+      <div class="uk-card-body uk-padding-small">
+          <p>{{ event.description }}</p>
+      </div>
+      <div class="uk-card-footer uk-padding-small">
+        <router-link v-bind:to="'/thread/' + event.id" class="uk-button uk-button-text">View thread</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +29,15 @@ export default {
   name: 'Event',
   props: {
     event: Object,
+  },
+  computed: {
+    formattedDate: function () {
+      const date = new Date(this.event.createdAt);
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const day = date.getDate() + 1;
+      return `${month}-${day}-${year}`
+    }
   },
 };
 </script>
