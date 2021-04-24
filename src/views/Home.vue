4d80d8138663675a2af94dc7e2b9c9ff987f1e91
cswaney <colinswaney@gmail.com>
2021-04-24 06:54:54 -0400

    Update Home.vue
    - Include Navbar and bind user info to it
    - Add updateLocation method and trigger on Navbar emit
    - Update createEvent method to upload attachment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,142 +1,148 @@
 <template>
-  <div class="uk-section uk-section-default uk-section-small">
-    <div class="uk-container uk-container-small">
+  <div id="root">
+    <Navbar v-bind:user="user" v-on:update-location="updateLocation"/>
+    <div class="uk-section uk-section-default uk-section-small">
+      <div class="uk-container uk-container-small">
 
-      <a class="uk-icon-button tm-icon-button" href="#" uk-icon="plus" uk-toggle></a>
-      <div id="add-event-dropdown" uk-dropdown="mode: click; pos: bottom-center;">
-          <form>
-              <div class="uk-margin-small-bottom">
-                  <input id="event-title-input" class="uk-input uk-form-width-medium" type="text" placeholder="Add event" v-model="title">
+        <a class="uk-icon-button tm-icon-button" href="#" uk-icon="plus" uk-toggle></a>
+        <div id="add-event-dropdown" uk-dropdown="mode: click; pos: bottom-center;">
+            <form>
+                <div class="uk-margin-small-bottom">
+                    <input id="event-title-input" class="uk-input uk-form-width-medium" type="text" placeholder="Add event" v-model="title">
+                </div>
+                <div class="uk-margin-small">
+                    <span class="uk-margin-small-right" uk-icon="clock"></span>
+                    <input id="event-input" class="uk-input uk-form-width-medium" type="text" placeholder="Date" v-model="date">
+                </div>
+                <div class="uk-margin-small">
+                    <span class="uk-margin-small-right" uk-icon="location"></span>
+                    <input id="event-input" class="uk-input uk-form-width-medium" type="text" placeholder="Address" v-model="address">
+                </div>
+                <div class="uk-margin-small uk-margin-remove-bottom">
+                    <span class="uk-margin-small-right" uk-icon="comment"></span>
+                    <input id="event-input" class="uk-input uk-form-width-medium" type="text" placeholder="Description" v-model="description">
+                </div>
+                <div class="uk-margin-small uk-margin-remove-bottom">
+                    <div uk-form-custom="target: true">
+                        <span class="uk-margin-small-right" uk-icon="file-text"></span>
+                        <input type="file" v-on:change="setAttachment">
+                        <input id="event-input" class="uk-input uk-form-width-medium" type="text" placeholder="Attachment" disabled>
+                    </div>
+                </div>
+                <hr class="uk-margin-small">
+                <div class="uk-margin-small uk-text-right">
+                    <a class="uk-icon-button tm-icon-button uk-margin-small-right" href="#" uk-icon="close" uk-toggle="target: #add-event-dropdown;"></a>
+                    <a id="event-check" class="uk-icon-button tm-icon-button" href="#" uk-icon="check" v-on:click="createEvent"></a>
+                </div>
+            </form>
+        </div>
+
+        <a class="uk-icon-button tm-icon-button uk-margin-small-left" href="#" uk-icon="refresh" v-on:click="getThreads"></a>
+
+        <!-- <img src="../assets/images/logo.jpg" class="uk-align-center uk-margin-remove-bottom" uk-svg width="280" height="200"/> -->
+        <img src="../assets/images/logo.png" class="uk-align-center uk-margin-remove-bottom" uk-svg width="280" height="200"/>
+        <h1 id="banner" class="uk-heading-small uk-text-center uk-margin-remove-top uk-margin-medium-bottom">{{ user.locationName }}</h1>
+
+        <!-- <p>{{ $auth.user }}</p> -->
+
+        <!-- <event v-for="thread in threads" :key="thread.id" v-bind:event="thread"></event> -->
+
+        <ul uk-accordion="multiple: true;">
+          <li v-for="thread in threads" v-bind:key="thread.id">
+              <a class="uk-accordion-title" href="#">
+                
+                {{ thread.name }}
+
+                <a v-if="thread.userId == user.id" class="uk-icon-button tm-icon-button uk-margin-small-left" href="#" uk-icon="pencil" uk-toggle></a>
+                <div id="edit-event-dropdown" uk-dropdown="mode: click; pos: top-center;">
+                    <form>
+                        <div class="uk-margin-small-bottom">
+                            <input id="event-title-input" class="uk-input uk-form-width-medium" type="text" :placeholder="thread.name" v-model="title">
+                        </div>
+                        <div class="uk-margin-small">
+                            <span class="uk-margin-small-right" uk-icon="clock"></span>
+                            <input id="event-input" class="uk-input uk-form-width-medium" type="text" :placeholder="thread.date" v-model="date">
+                        </div>
+                        <div class="uk-margin-small">
+                            <span class="uk-margin-small-right" uk-icon="location"></span>
+                            <input id="event-input" class="uk-input uk-form-width-medium" type="text" :placeholder="thread.address" v-model="address">
+                        </div>
+                        <div class="uk-margin-small uk-margin-remove-bottom">
+                            <span class="uk-margin-small-right" uk-icon="comment"></span>
+                            <input id="event-input" class="uk-input uk-form-width-medium" type="text" :placeholder="thread.description" v-model="description">
+                        </div>
+                        <div class="uk-margin-small uk-margin-remove-bottom">
+                            <div uk-form-custom="target: true">
+                                <span class="uk-margin-small-right" uk-icon="file-text"></span>
+                                <input type="file" v-on:change="setAttachment">
+                                <input id="event-input" class="uk-input uk-form-width-medium" type="text" placeholder="Attachment" disabled>
+                            </div>
+                        </div>
+                        <hr class="uk-margin-small">
+                        <div class="uk-margin-small uk-text-right">
+                            <a class="uk-icon-button tm-icon-button uk-margin-small-right" href="#" uk-icon="close" uk-toggle="target: #edit-event-dropdown;"></a>
+                            <a id="event-check" class="uk-icon-button tm-icon-button" href="#" uk-icon="check" v-on:click="updateEvent"></a>
+                        </div>
+                    </form>
+                </div>
+
+                <a v-if="thread.userId == user.id" class="uk-icon-button tm-icon-button uk-margin-small-left" href="#" uk-icon="trash" uk-toggle></a>
+                <div id="delete-event-dropdown" uk-dropdown="mode: click; pos: top-center;">
+                    <form>
+                        <div class="uk-margin-small">
+                            <span class="uk-margin-small-right" uk-icon="warning"></span>
+                            <input id="event-input" class="uk-input uk-form-width-large" type="text" placeholder="Type DELETE to confirm">
+                        </div>
+                        <hr class="uk-margin-small">
+                        <div class="uk-margin-small uk-text-right">
+                            <a class="uk-icon-button tm-icon-button uk-margin-small-right" href="#" uk-icon="close" uk-toggle="target: #delete-event-dropdown;"></a>
+                            <a id="event-warn" class="uk-icon-button tm-icon-button" href="#" uk-icon="check" v-on:click="deleteEvent(thread.id)"></a>
+                        </div>
+                    </form>
+                </div>
+              </a>
+              <div class="uk-accordion-content">
+                  <ul class="uk-list">
+                    <li><b>What</b> {{ thread.description }} <router-link v-bind:to="'/thread/' + thread.id" class="uk-button uk-button-text">Details</router-link></li>
+                    <li><b>When</b> {{ thread.date }}</li>
+                    <li><b>Where</b> {{ thread.address }}</li>
+                  </ul>
               </div>
-              <div class="uk-margin-small">
-                  <span class="uk-margin-small-right" uk-icon="clock"></span>
-                  <input id="event-input" class="uk-input uk-form-width-medium" type="text" placeholder="Date" v-model="date">
-              </div>
-              <div class="uk-margin-small">
-                  <span class="uk-margin-small-right" uk-icon="location"></span>
-                  <input id="event-input" class="uk-input uk-form-width-medium" type="text" placeholder="Address" v-model="address">
-              </div>
-              <div class="uk-margin-small uk-margin-remove-bottom">
-                  <span class="uk-margin-small-right" uk-icon="comment"></span>
-                  <input id="event-input" class="uk-input uk-form-width-medium" type="text" placeholder="Description" v-model="description">
-              </div>
-              <div class="uk-margin-small uk-margin-remove-bottom">
-                  <div uk-form-custom="target: true">
-                      <span class="uk-margin-small-right" uk-icon="file-text"></span>
-                      <input type="file" v-on:change="setAttachment">
-                      <input id="event-input" class="uk-input uk-form-width-medium" type="text" placeholder="Attachment" disabled>
-                  </div>
-              </div>
-              <hr class="uk-margin-small">
-              <div class="uk-margin-small uk-text-right">
-                  <a class="uk-icon-button tm-icon-button uk-margin-small-right" href="#" uk-icon="close" uk-toggle="target: #add-event-dropdown;"></a>
-                  <a id="event-check" class="uk-icon-button tm-icon-button" href="#" uk-icon="check" v-on:click="createEvent"></a>
-              </div>
-          </form>
+          </li>
+        </ul>
+
+        <ul class="uk-pagination uk-flex-center" uk-margin>
+          <li><a href="#"><span uk-pagination-previous></span></a></li>
+          <li><a href="#">1</a></li>
+          <li class="uk-disabled"><span>...</span></li>
+          <li><a href="#">4</a></li>
+          <li><a href="#">5</a></li>
+          <li><a href="#">6</a></li>
+          <li class="uk-active"><span>7</span></li>
+          <li><a href="#">8</a></li>
+          <li><a href="#">9</a></li>
+          <li><a href="#">10</a></li>
+          <li class="uk-disabled"><span>...</span></li>
+          <li><a href="#">20</a></li>
+          <li><a href="#"><span uk-pagination-next></span></a></li>
+        </ul>
+
       </div>
-
-      <a class="uk-icon-button tm-icon-button uk-margin-small-left" href="#" uk-icon="refresh" v-on:click="getThreads"></a>
-
-      <!-- <img src="../assets/images/logo.jpg" class="uk-align-center uk-margin-remove-bottom" uk-svg width="280" height="200"/> -->
-      <img src="../assets/images/logo.png" class="uk-align-center uk-margin-remove-bottom" uk-svg width="280" height="200"/>
-      <h1 id="banner" class="uk-heading-small uk-text-center uk-margin-remove-top uk-margin-medium-bottom">{{ user.locationName }}</h1>
-
-      <!-- <p>{{ $auth.user }}</p> -->
-
-      <!-- <event v-for="thread in threads" :key="thread.id" v-bind:event="thread"></event> -->
-
-      <ul uk-accordion="multiple: true;">
-        <li v-for="thread in threads" v-bind:key="thread.id">
-            <a class="uk-accordion-title" href="#">
-              
-              {{ thread.name }}
-
-              <a v-if="thread.userId == user.userId" class="uk-icon-button tm-icon-button uk-margin-small-left" href="#" uk-icon="pencil" uk-toggle></a>
-              <div id="edit-event-dropdown" uk-dropdown="mode: click; pos: top-center;">
-                  <form>
-                      <div class="uk-margin-small-bottom">
-                          <input id="event-title-input" class="uk-input uk-form-width-medium" type="text" :placeholder="thread.name" v-model="title">
-                      </div>
-                      <div class="uk-margin-small">
-                          <span class="uk-margin-small-right" uk-icon="clock"></span>
-                          <input id="event-input" class="uk-input uk-form-width-medium" type="text" :placeholder="thread.date" v-model="date">
-                      </div>
-                      <div class="uk-margin-small">
-                          <span class="uk-margin-small-right" uk-icon="location"></span>
-                          <input id="event-input" class="uk-input uk-form-width-medium" type="text" :placeholder="thread.address" v-model="address">
-                      </div>
-                      <div class="uk-margin-small uk-margin-remove-bottom">
-                          <span class="uk-margin-small-right" uk-icon="comment"></span>
-                          <input id="event-input" class="uk-input uk-form-width-medium" type="text" :placeholder="thread.description" v-model="description">
-                      </div>
-                      <div class="uk-margin-small uk-margin-remove-bottom">
-                          <div uk-form-custom="target: true">
-                              <span class="uk-margin-small-right" uk-icon="file-text"></span>
-                              <input type="file" v-on:change="setAttachment">
-                              <input id="event-input" class="uk-input uk-form-width-medium" type="text" placeholder="Attachment" disabled>
-                          </div>
-                      </div>
-                      <hr class="uk-margin-small">
-                      <div class="uk-margin-small uk-text-right">
-                          <a class="uk-icon-button tm-icon-button uk-margin-small-right" href="#" uk-icon="close" uk-toggle="target: #edit-event-dropdown;"></a>
-                          <a id="event-check" class="uk-icon-button tm-icon-button" href="#" uk-icon="check" v-on:click="updateEvent"></a>
-                      </div>
-                  </form>
-              </div>
-
-              <a v-if="thread.userId == user.userId" class="uk-icon-button tm-icon-button uk-margin-small-left" href="#" uk-icon="trash" uk-toggle></a>
-              <div id="delete-event-dropdown" uk-dropdown="mode: click; pos: top-center;">
-                  <form>
-                      <div class="uk-margin-small">
-                          <span class="uk-margin-small-right" uk-icon="warning"></span>
-                          <input id="event-input" class="uk-input uk-form-width-large" type="text" placeholder="Type DELETE to confirm">
-                      </div>
-                      <hr class="uk-margin-small">
-                      <div class="uk-margin-small uk-text-right">
-                          <a class="uk-icon-button tm-icon-button uk-margin-small-right" href="#" uk-icon="close" uk-toggle="target: #delete-event-dropdown;"></a>
-                          <a id="event-warn" class="uk-icon-button tm-icon-button" href="#" uk-icon="check" v-on:click="deleteEvent(thread.id)"></a>
-                      </div>
-                  </form>
-              </div>
-            </a>
-            <div class="uk-accordion-content">
-                <ul class="uk-list">
-                  <li><b>What</b> {{ thread.description }} <router-link v-bind:to="'/thread/' + thread.id" class="uk-button uk-button-text">Details</router-link></li>
-                  <li><b>When</b> {{ thread.date }}</li>
-                  <li><b>Where</b> {{ thread.address }}</li>
-                </ul>
-            </div>
-        </li>
-      </ul>
-
-      <ul class="uk-pagination uk-flex-center" uk-margin>
-        <li><a href="#"><span uk-pagination-previous></span></a></li>
-        <li><a href="#">1</a></li>
-        <li class="uk-disabled"><span>...</span></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
-        <li><a href="#">6</a></li>
-        <li class="uk-active"><span>7</span></li>
-        <li><a href="#">8</a></li>
-        <li><a href="#">9</a></li>
-        <li><a href="#">10</a></li>
-        <li class="uk-disabled"><span>...</span></li>
-        <li><a href="#">20</a></li>
-        <li><a href="#"><span uk-pagination-next></span></a></li>
-      </ul>
-
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-// import Event from '@/components/Event.vue'; // @ is an alias to /src
 import { apiEndpoint } from '../config';
+
+import Navbar from '@/components/Navbar.vue';
+// import Event from '@/components/Event.vue'; // @ is an alias to /src
 
 export default {
   name: 'Home',
   components: {
+    Navbar,
     // Event,
   },
   data() {
@@ -180,6 +186,9 @@ export default {
     },
     authUserId() {
       return this.$auth.user.sub.split('|')[1];
+    },
+    auth() {
+      return this.$auth;
     }
   },
   methods: {
@@ -218,7 +227,7 @@ export default {
         locationName: this.user.locationName,
         address: this.address,
         date: this.date,
-        userId: this.user.userId
+        userId: this.user.id
       }
       const { data } = await axios.post(url, JSON.stringify(event), {
         headers: {
@@ -228,6 +237,11 @@ export default {
       });
       console.log('done.');
       console.log('created event:', data.event);
+      const signedUrl = data.signedUrl;
+      console.log(`uploading attachment (url=${signedUrl})`);
+      await axios.put(signedUrl, this.attachment);
+      this.attachment = '';  // remove attachment
+      console.log('done.');
     },
     async updateEvent() {
       console.log('editing event...');
@@ -245,7 +259,36 @@ export default {
       console.log('done.');
       console.log('deleted event:', data.event);
     },
+    async updateLocation(selectedLocation) {
+      if (selectedLocation != this.user.locationName) {
+        console.log(`updating settings (location=${selectedLocation})...`);
+        const token = await this.$auth.getTokenSilently();  // TODO: can we reuse the same token? Store in component data???
+        const request = {
+          locationId: selectedLocation.id,
+          locationName: selectedLocation.name,
+          name: this.user.name,
+          avatarUrl: this.user.avatarUrl
+        };
+        const url = apiEndpoint + `/user/${this.user.id}`;
+        const { data } = await axios.patch(url, request, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`, // send the access token through the Auth header
+          }
+        });
+        // if succesful, update user locationId and locationName
+        console.log('done.');
+        this.user.locationId = selectedLocation.id;
+        this.user.locationName = selectedLocation.name;
+        console.log('updated user:', data.user);
+
+        // TODO: else, indicate error
+        // ...
+        // console.log('done.');
+      }
+    },
     setAttachment(event) {
+      // TODO: find a way to do this in createEvent/updateEvent to make sure attachment is added before creating/updating?
       console.log('setting attachment: ', event.target.files);
       this.attachment = event.target.files[0];
     }
